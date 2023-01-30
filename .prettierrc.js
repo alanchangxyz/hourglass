adcadc0e0d7958dc01fbc2dbf951d4dc423dd5b0
Alan Chang <alan@alanchang.xyz>
2023-01-30 01:47:48 -0800

    Merge prettiercs

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,13 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
   singleQuote: true,
+  tabWidth: 2,
   trailingComma: 'all',
+  useTabs: false
 };
