e61febc9e5e700becccc517828ec6b5afe534df9
Alan Chang <alan@alanchang.xyz>
2023-01-30 04:05:39 -0800

    Fix eslintrc and extend to airbnb conventions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,19 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'airbnb',
+    'plugin:import/warnings',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      experimentalObjectRestSpread: true,
+      modules: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
 };
