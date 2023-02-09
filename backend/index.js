95cb0682ce29e6ea83690fbb32f0ab3f814b71ed
Alan Chang <alan@alanchang.xyz>
2023-02-08 20:08:57 -0800

    Setup first version of Node backend

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: `*`,
+  }),
+);
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+module.exports = app;
