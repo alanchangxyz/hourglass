909ce292f103dac1a7f747859f043cb3d2bd442d
Alan Chang <alan@alanchang.xyz>
2023-02-19 03:35:37 -0800

    Add returns on everything

--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -4,27 +4,27 @@ const { db } = require('../server/db');
 const tasksRouter = express();
 
 tasksRouter.get('/', async (req, res) => {
-  res.status(200).send('ping - tasks get all');
+  return res.status(200).send('ping - tasks get all');
 });
 
 tasksRouter.get('/:id', async (req, res) => {
   const { id } = req.params;
-  res.status(200).send('ping - tasks get one');
+  return res.status(200).send('ping - tasks get one');
 });
 
 tasksRouter.post('/', async (req, res) => {
   const { id } = req.body;
-  res.status(200).send('ping - tasks post');
+  return res.status(200).send('ping - tasks post');
 });
 
 tasksRouter.put('/:id', async (req, res) => {
   const { id } = req.params;
-  res.status(200).send('ping - tasks put');
+  return res.status(200).send('ping - tasks put');
 });
 
 tasksRouter.put('/:', async (req, res) => {
   const { id } = req.params;
-  res.status(200).send('ping - tasks delete');
+  return res.status(200).send('ping - tasks delete');
 });
 
 module.exports = tasksRouter;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,27 +4,27 @@ const { db } = require('../server/db');
 const usersRouter = express();
 
 usersRouter.get('/', async (req, res) => {
-  res.status(200).send('ping - users get all');
+  return res.status(200).send('ping - users get all');
 });
 
 usersRouter.get('/:id', async (req, res) => {
   const { id } = req.params;
-  res.status(200).send('ping - users get one');
+  return res.status(200).send('ping - users get one');
 });
 
 usersRouter.post('/', async (req, res) => {
   const { id } = req.body;
-  res.status(200).send('ping - users post');
+  return res.status(200).send('ping - users post');
 });
 
 usersRouter.put('/:id', async (req, res) => {
   const { id } = req.params;
-  res.status(200).send('ping - users put');
+  return res.status(200).send('ping - users put');
 });
 
 usersRouter.put('/:', async (req, res) => {
   const { id } = req.params;
-  res.status(200).send('ping - users delete');
+  return res.status(200).send('ping - users delete');
 });
 
 module.exports = usersRouter;
