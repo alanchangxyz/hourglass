7139a9b919e4628fb75f2cf550fe7e1817346ad5
Alan Chang <alan@alanchang.xyz>
2023-02-19 03:34:47 -0800

    Make pings on skeleton placeholder routes more specific

--- backend/routes/recommendations.js
+++ backend/routes/recommendations.js
@@ -4,27 +4,27 @@ const { db } = require('../server/db');
 const recommendationsRouter = express();
 
 recommendationsRouter.get('/', async (req, res) => {
-  res.status(200).send('ping');
+  res.status(200).send('ping - recs get all');
 });
 
 recommendationsRouter.get('/:id', async (req, res) => {
   const { id } = req.params;
-  res.status(200).send('ping');
+  res.status(200).send('ping - recs get one');
 });
 
 recommendationsRouter.post('/', async (req, res) => {
   const { id } = req.body;
-  res.status(200).send('ping');
+  res.status(200).send('ping - recs post');
 });
 
 recommendationsRouter.put('/:id', async (req, res) => {
   const { id } = req.params;
-  res.status(200).send('ping');
+  res.status(200).send('ping - recs put');
 });
 
 recommendationsRouter.put('/:', async (req, res) => {
   const { id } = req.params;
-  res.status(200).send('ping');
+  res.status(200).send('ping - recs delete');
 });
 
 module.exports = recommendationsRouter;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,27 +4,27 @@ const { db } = require('../server/db');
 const usersRouter = express();
 
 usersRouter.get('/', async (req, res) => {
-  res.status(200).send('ping');
+  res.status(200).send('ping - users get all');
 });
 
 usersRouter.get('/:id', async (req, res) => {
   const { id } = req.params;
-  res.status(200).send('ping');
+  res.status(200).send('ping - users get one');
 });
 
 usersRouter.post('/', async (req, res) => {
   const { id } = req.body;
-  res.status(200).send('ping');
+  res.status(200).send('ping - users post');
 });
 
 usersRouter.put('/:id', async (req, res) => {
   const { id } = req.params;
-  res.status(200).send('ping');
+  res.status(200).send('ping - users put');
 });
 
 usersRouter.put('/:', async (req, res) => {
   const { id } = req.params;
-  res.status(200).send('ping');
+  res.status(200).send('ping - users delete');
 });
 
 module.exports = usersRouter;
