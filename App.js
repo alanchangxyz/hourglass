c5402f79da349277ade02102502b5df074e9990f
Parsa Karamat <pkmtyp@gmail.com>
2023-02-19 16:30:24 -0800

    Added Navigation Bar and temporary views

--- App.js
+++ App.js
@@ -2,26 +2,59 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NativeBaseProvider} from 'native-base';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {Text} from 'react-native';
 
-import Home from './src/views/Home';
+// Views
+import HomeView from './src/views/Home';
+import ScheduleView from './src/views/Schedule';
+import TasksView from './src/views/Tasks';
+import ProfileView from './src/views/Profile';
 
-const Stack = createNativeStackNavigator();
+//View names
+const home = "Home";
+const schedule = "Schedule";
+const tasks = "Tasks";
+const profile = "Profile";
 
-// const App: () => Node = () => {
-const App = () => {
+// Navigation Tab
+const Tab = createBottomTabNavigator();
+
+function NavigationBar() {
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{title: 'Welcome'}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={home}
+        screenOptions={({ route }) => ({
+          tabBarIcon: () => {
+            if (route.name === home) {
+              return <Text>Home Icon</Text>;
+            }
+            else if (route.name === schedule) {
+              return <Text>Schedule Icon</Text>
+            }
+            else if (route.name === tasks) {
+              return <Text>Tasks Icon</Text>
+            }
+            else if (route.name === profile) {
+              return <Text>Profile Icon</Text>;
+            }
+          },
+        })}
+        >
+
+        <Tab.Screen name={home} component={HomeView} />
+        <Tab.Screen name={schedule} component={ScheduleView} />
+        <Tab.Screen name={tasks} component={TasksView} />
+        <Tab.Screen name={profile} component={ProfileView} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
   );
+}
+
+const App = () => {
+  return (<NavigationBar/>);
 };
 
 export default App;
---
+++ src/views/Profile.jsx
@@ -0,0 +1,83 @@
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+// const Section = ({children, title}): Node => {
+const Section = ({children, title}) => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const Profile = () => {
+  const [name, setName] = useState('user');
+
+  return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text>Try editing me! 🎉</Text>
+      </View>
+    );
+};
+
+export default Profile;
