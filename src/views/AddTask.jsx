1e594e1f8e79479205859072c6fe4099c30e9b7e
Olivia Ih <ih.olivia.e@gmail.com>
2023-02-19 18:15:30 -0800

    add task page basic layout and styling

--- src/views/AddTask.jsx
+++ src/views/AddTask.jsx
@@ -10,14 +10,79 @@ import {
     TextInput
 } from 'react-native';
 
-const AddTaskView = ({navigation}) => {
+
+const styles = StyleSheet.create({
+    fieldTitle: {
+        fontWeight: 400,
+        fontSize: 14,
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+        color: 'black'
+    },
+    textInput: {
+        borderWidth: 1,
+        marginHorizontal: 20,
+        padding: 10
+    },
+    taskDurationFields: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingBottom: 30,
+    },
+});
+
+const TaskNameField = () => {
+    const [taskName, setTaskName] = useState("");
+    return (
+        <View>
+            <Text style={styles.fieldTitle}>Task Name</Text>
+            <TextInput 
+                style={styles.textInput} 
+                placeholder="Task Name"
+                onChangeText={newText => setTaskName(newText)}
+                defaultValue={taskName}
+            />
+        </View>
+    );
+};
+
+const TaskDurationField = () => {
+    const [taskHours, setTaskHours] = useState("0");
+    const [taskMins, setTaskMins] = useState("0");
+    return (
+        <View>
+            <Text style={styles.fieldTitle}>Task Duration</Text>
+            <View style={styles.taskDurationFields}>
+                <TextInput 
+                    style={styles.textInput}
+                    placeholder="Hours"
+                    onChangeText={newText => setTaskHours(newText)}
+                    defaultValue={taskHours}
+                    inputMode="numeric"
+                />
+                <Text>Hours</Text>
+                <TextInput 
+                    style={styles.textInput}
+                    placeholder="Minutes"
+                    onChangeText={newText => setTaskMins(newText)}
+                    defaultValue={taskMins}
+                    inputMode="numeric"
+                />
+                <Text>Minutes</Text>
+            </View>
+            
+        </View>
+    );
+};
+
+const AddTaskView = ({navigation, route}) => {
     return (
         <SafeAreaView>
             <StatusBar />
             <ScrollView contentInsetAdjustmentBehavior="automatic">
-                <Text>Task Name</Text>
-                <TextInput placeholder="Task Name"/>
-                <Text>Task Duration</Text>
+                <TaskNameField />
+                <TaskDurationField />
+                <Button title="Add Task"/>
             </ScrollView>
         </SafeAreaView>
     );
