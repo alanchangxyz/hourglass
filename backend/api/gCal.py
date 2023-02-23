a6b6655e32b9325ff81a1445b0ee4935befd3171
Parsa Karamat <pkmtyp@gmail.com>
2023-02-22 23:47:00 -0800

    Added Google Calendar to backend

---
+++ backend/api/gCal.py
@@ -0,0 +1,44 @@
+import os.path, os
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SUCCESS_MESSAGE = "Hourglass has successfully connected to your Google Calendar"
+
+
+def credential_authorization():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(os.getenv('GCAL_TOKEN')):
+        creds = Credentials.from_authorized_user_file(os.getenv('GCAL_TOKEN'), SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                os.getenv('GCAL_CREDS'), SCOPES)
+            creds = flow.run_local_server(port=0, success_message=SUCCESS_MESSAGE)
+        # Save the credentials for the next run
+        with open(os.getenv('GCAL_TOKEN'), 'w') as token:
+            token.write(creds.to_json())
+
+def request_events(start_time, end_time):
+    creds = None
+    if os.path.exists(os.getenv('GCAL_TOKEN')):
+        creds = Credentials.from_authorized_user_file(os.getenv('GCAL_TOKEN'), SCOPES)
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+        events = service.events().list(calendarId='primary', timeMin=start_time,
+                                              timeMax= end_time,
+                                              singleEvents=True,
+                                              orderBy='startTime').execute()
+        return events["items"]
+    except HttpError as error:
+        print('Error: %s' % error)
--- backend/api/index.py
+++ backend/api/index.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
 from psycopg2.errors import UniqueViolation
 from db import query, connection, cursor
+from gCal import credential_authorization, request_events
 import util
 
 import os
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -117,6 +119,18 @@ def users_delete_one(id):
   except:
     return util.error500("Internal server error")
 
+
+# Google Calendar Credential Authorization
+@app.route('/authorize', methods=['GET'])
+def google_authorization():
+  return credential_authorization()
+
+# Google Calendar Credential Authorization
+@app.route('/calendar/<date>', methods=['GET'])
+def calendar_get_one(date):
+  start_date = datetime.strptime(date,"%m-%d-%Y").isoformat() + 'Z'
+  end_date = (datetime.strptime(date,"%m-%d-%Y") + timedelta(days = 1)).isoformat() + 'Z'
+  return request_events(start_date, end_date)
 
 if __name__ == '__main__':
   app.run(host="localhost", port=os.getenv('FLASK_RUN_PORT'), debug=os.getenv('FLASK_RUN_DEBUG'))
