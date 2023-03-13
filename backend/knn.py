f83af30a012ede17501fa85cebd598c35ceeee69
Parsa Karamat <pkmtyp@gmail.com>
2023-03-12 18:11:27 -0700

    Updated requesting calendar and step size calc

--- backend/knn.py
+++ backend/knn.py
@@ -8,12 +8,11 @@ from datetime import datetime, timedelta
 import json
 import random
 from db import query_pandas
+from gcal import request_events
 
-import requests
+import pytz
 import numpy as np
 from sklearn.neighbors import KNeighborsRegressor
-
-API_URL = "https://hourglass.alanchang.xyz/api"
 
 def get_recommendations(tid):
     get_recommendation_query = f"SELECT * FROM recommendations WHERE tid = {tid};"
@@ -23,7 +22,7 @@ def get_task_window(tid):
     get_task_window_query = f"SELECT start_range, end_range, duration FROM tasks WHERE tid = {tid};"
     return query_pandas(get_task_window_query)
 
-def create_time_lists(start_time, end_time, duration, step_size = 10):
+def create_time_lists(start_time, end_time, duration, step_size):
     duration = timedelta(minutes=int(duration))
     start_time = datetime.strptime(start_time, "%H:%M:%S")
     end_time = datetime.strptime(end_time, "%H:%M:%S") - duration
@@ -48,9 +47,23 @@ def get_task_data(tid):
     df_tasks["end_range"] = df_tasks['end_range'].dt.time
     return df_tasks
 
+def generate_step_size(duration, start_range, end_range):
+    duration = timedelta(minutes=int(duration))
+    start_range = datetime.strptime(start_range, "%H:%M:%S")
+    end_range = datetime.strptime(end_range, "%H:%M:%S") - duration
+    time_diff = end_range - start_range
+    max_step_size = (time_diff.total_seconds() / 60) / 5
+    if max_step_size >= 10:
+        return 10
+    elif max_step_size >= 5:
+        return 5
+    else:
+        return int(max_step_size)
+
 def get_lists_to_rank(df_tasks, start_range, end_range):
     duration = df_tasks["duration"].astype("string")[0]
-    times, times_seconds = create_time_lists(start_range, end_range, duration)
+    step = generate_step_size(duration, start_range, end_range)
+    times, times_seconds = create_time_lists(start_range, end_range, duration, step_size= step)
     return (times, np.array(times_seconds).reshape(-1, 1))
 
 def get_unfiltered_ranking(tid, start_range, end_range):
@@ -91,14 +104,21 @@ def convert_to_json(ranking):
 
     return json.dumps(rankingsList)
 
+def get_calendar_data(date):
+  start_date_pst = datetime.strptime(f"{date} 00:00:00","%m-%d-%Y %H:%M:%S")
+  end_date_pst = datetime.strptime(f"{date} 00:00:00","%m-%d-%Y %H:%M:%S")+ timedelta(days = 1)
+  pst_time = pytz.timezone("America/Los_Angeles")
+  start_date_utc = pst_time.localize(start_date_pst, is_dst=None).astimezone(pytz.utc).isoformat().replace("+00:00", "Z")
+  end_date_utc = pst_time.localize(end_date_pst, is_dst=None).astimezone(pytz.utc).isoformat().replace("+00:00", "Z")
+  return request_events(start_date_utc, end_date_utc)
+
 def filter_by_calendar(unfiltered_rankings, date):
     # minutes scheduled
     minutes_scheduled = set()
 
     # Date in the format M/D/YEAR
     date = date.replace("/", "-")
-    response = requests.get(f"{API_URL}/calendar/{date}")
-    body = response.json()
+    body = get_calendar_data(date)
     for item in body:
         start_hour, start_minute, _ = item["start"]["dateTime"].split("T")[1].split("-")[0].split(":")
         scheduled_start_minutes = int(start_hour) * 60 + int(start_minute)
@@ -126,13 +146,18 @@ def filter_by_calendar(unfiltered_rankings, date):
     recommendations_ranked = nonoverlapping_times + overlapping_times
     return recommendations_ranked
 
+def rankings_top_percent(rankings, percent):
+    num_recs = int(len(rankings) * percent)
+    return [rankings[i] for i in range(num_recs)]
+
 
 def get_ranking(tid, start_date, end_date):
     date, start_range = start_date.split(" ")
     _, end_range = end_date.split(" ")
     unfiltered_rankings = get_unfiltered_ranking(tid, start_range, end_range)
     filtered_rankings = filter_by_calendar(unfiltered_rankings, date)
-    return convert_to_json(filtered_rankings)
+    top_rankings = rankings_top_percent(filtered_rankings, percent = 0.20)
+    return convert_to_json(top_rankings)
 
 
 if __name__ == "__main__":
