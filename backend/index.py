83958ab0434773940bb26c05dca4900006990b78
Parsa Karamat <pkmtyp@gmail.com>
2023-03-19 15:16:40 -0700

    removed min_offset from post request

--- backend/index.py
+++ backend/index.py
@@ -96,12 +96,12 @@ def recs_get_homepage(id):
 
 @app.route('/recommendations', methods=['POST'])
 def recommendations_create_one():
-  js = request.get_json() #start_time, #end_time, min_offset, chosen, added_to_cal, uid, tid
+  js = request.get_json() #start_time, #end_time, chosen, added_to_cal, uid, tid
 
   try:
     cursor.execute(f'INSERT INTO recommendations\
-      (start_time, end_time, min_offset, chosen, added_to_cal, uid, tid)\
-      VALUES ((TIMESTAMP %s), (TIMESTAMP %s), %s, %s, %s, %s, %s) RETURNING *', (js['start_time'], js['end_time'], js['min_offset'], js['chosen'], js['added_to_cal'], js['uid'], js['tid']))
+      (start_time, end_time, chosen, added_to_cal, uid, tid)\
+      VALUES ((TIMESTAMP %s), (TIMESTAMP %s), %s, %s, %s, %s) RETURNING *', (js['start_time'], js['end_time'], js['chosen'], js['added_to_cal'], js['uid'], js['tid']))
     connection.commit()
     res = cursor.fetchone()
     res['rid'] = str(res['rid'])
